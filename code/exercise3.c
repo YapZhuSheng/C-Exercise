5b0782d24b7b6ec06512d6319b8c87579fb58345
Zhu Sheng <51312758+YapZhuSheng@users.noreply.github.com>
2022-09-03 21:39:35 +0800

    Create exercise3.c

---
+++ code/exercise3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+	printf("<----- C Programming ----->\n");
+	printf("<                         >\n");
+	printf("<      How do you do?     >\n");
+	printf("<                         >\n");
+	printf("<     Nice to meet you!   >\n");
+	printf("<                         >\n");
+	printf("<        Good Luck!       >\n");
+	return 0;
+}
