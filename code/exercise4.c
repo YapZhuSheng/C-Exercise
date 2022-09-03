b5abe663ecc7977ef98346067751cdd626b402dd
Zhu Sheng <51312758+YapZhuSheng@users.noreply.github.com>
2022-09-03 21:49:23 +0800

    Create exercise4.c

---
+++ code/exercise4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+	printf("**************************************************\n");
+	printf("*                                                *\n");
+	printf("*             Welcome to my program              *\n");
+	printf("*                                                *\n");
+	printf("*                 1. New Game                    *\n");
+	printf("*                 2. Existing Game               *\n");
+	printf("*                 3. Save Game                   *\n");
+	printf("*                 4. Exit                        *\n");
+	printf("*                                                *\n");
+	printf("**************************************************\n");
+	
+	return 0;
+	
+}
